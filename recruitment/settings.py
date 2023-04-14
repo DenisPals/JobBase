1831c0f2c2097d20147bdaa0bf367f8b14cd60df
Denis Pals <palsdenisworking@yahoo.com>
2023-04-14 03:11:14 +0100

    StaticRoot modified

--- recruitment/settings.py
+++ recruitment/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'https://storage.googleapis.com/379368953291/static/'
 
 # Base url to serve media files
 MEDIA_URL = '/media/'
