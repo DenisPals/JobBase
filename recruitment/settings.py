e137c5ac1cdabcd0acd81668e90a60715a98d4ef
Denis Pals <palsdenisworking@yahoo.com>
2023-04-15 16:34:05 +0100

    newTry

--- recruitment/settings.py
+++ recruitment/settings.py
@@ -19,7 +19,8 @@ from django.test.runner import DiscoverRunner
 BASE_DIR = Path(__file__).resolve().parent.parent
 """
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
