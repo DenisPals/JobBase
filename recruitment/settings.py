501b31985fe279cd618142640a8dc4f52bc68e8b
Denis Pals <palsdenisworking@yahoo.com>
2023-04-15 17:28:51 +0100

    newTry

--- recruitment/settings.py
+++ recruitment/settings.py
@@ -143,6 +143,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'https://storage.googleapis.com/379368953291/static/'
 
 from google.oauth2 import service_account
