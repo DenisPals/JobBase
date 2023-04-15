ac1ab67cc5120c0014e0d7bf3fca0fd230332a01
Denis Pals <palsdenisworking@yahoo.com>
2023-04-15 16:36:21 +0100

    newTry

--- recruitment/settings.py
+++ recruitment/settings.py
@@ -19,8 +19,7 @@ from django.test.runner import DiscoverRunner
 BASE_DIR = Path(__file__).resolve().parent.parent
 """
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(__file__).resolve().parent.parent
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -135,7 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+#STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = "static/"
 
 # Enable WhiteNoise's GZip compression of static assets.
